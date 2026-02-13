99da4074bb3839d2b91791ebe0fc27e49a4a6910
Michał Wyczarski <mwyczarski@gmail.com>
2026-02-13 15:52:38 +0100

    Fixes formatting issues in exports
    Removes unnecessary line breaks in export statements for improved code readability and consistency.

--- lib/mt_carplay.dart
+++ lib/mt_carplay.dart
@@ -1,18 +1,14 @@
 export 'package:mt_carplay/aa_models/list/list_item.dart' show AAListItem;
-export 'package:mt_carplay/aa_models/list/list_section.dart'
-    show AAListSection;
+export 'package:mt_carplay/aa_models/list/list_section.dart' show AAListSection;
 export 'package:mt_carplay/aa_models/list/list_template.dart'
     show AAListTemplate;
-export 'package:mt_carplay/android_auto_worker.dart'
-    show FlutterAndroidAuto;
+export 'package:mt_carplay/android_auto_worker.dart' show FlutterAndroidAuto;
 export 'package:mt_carplay/carplay_worker.dart' show FlutterCarplay;
-export 'package:mt_carplay/constants/constants.dart'
-    show ConnectionStatusTypes;
+export 'package:mt_carplay/constants/constants.dart' show ConnectionStatusTypes;
 export 'package:mt_carplay/helpers/enum_utils.dart';
 export 'package:mt_carplay/models/action_sheet/action_sheet_template.dart'
     show CPActionSheetTemplate;
-export 'package:mt_carplay/models/alert/alert_action.dart'
-    show CPAlertAction;
+export 'package:mt_carplay/models/alert/alert_action.dart' show CPAlertAction;
 export 'package:mt_carplay/models/alert/alert_constants.dart'
     show CPAlertActionStyles;
 export 'package:mt_carplay/models/alert/alert_template.dart'
@@ -20,11 +16,9 @@ export 'package:mt_carplay/models/alert/alert_template.dart'
 export 'package:mt_carplay/models/button/alert_constants.dart'
     show CPBarButtonStyles;
 export 'package:mt_carplay/models/button/bar_button.dart' show CPBarButton;
-export 'package:mt_carplay/models/button/text_button.dart'
-    show CPTextButton;
+export 'package:mt_carplay/models/button/text_button.dart' show CPTextButton;
 export 'package:mt_carplay/models/grid/grid_button.dart' show CPGridButton;
-export 'package:mt_carplay/models/grid/grid_template.dart'
-    show CPGridTemplate;
+export 'package:mt_carplay/models/grid/grid_template.dart' show CPGridTemplate;
 export 'package:mt_carplay/models/information/information_constants.dart'
     show CPInformationTemplateLayout;
 export 'package:mt_carplay/models/information/information_item.dart'
@@ -34,10 +28,8 @@ export 'package:mt_carplay/models/information/information_template.dart'
 export 'package:mt_carplay/models/list/list_constants.dart'
     show CPListItemAccessoryTypes, CPListItemPlayingIndicatorLocations;
 export 'package:mt_carplay/models/list/list_item.dart' show CPListItem;
-export 'package:mt_carplay/models/list/list_section.dart'
-    show CPListSection;
-export 'package:mt_carplay/models/list/list_template.dart'
-    show CPListTemplate;
+export 'package:mt_carplay/models/list/list_section.dart' show CPListSection;
+export 'package:mt_carplay/models/list/list_template.dart' show CPListTemplate;
 export 'package:mt_carplay/models/poi/poi.dart' show CPPointOfInterest;
 export 'package:mt_carplay/models/poi/poi_template.dart'
     show CPPointOfInterestTemplate;
